67612f44cebffd9a0eeac638927c6545aa8862b0
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2024-08-03 23:03:57 +0700

    feat: cf func

---
+++ functions/w2g/[id].js
@@ -0,0 +1,33 @@
+// this is a bit scuffed, but these are cf pages redirect functions
+
+/** @type {PagesFunction} */
+export function onRequest ({ params }) {
+  try {
+    const id = params.id
+    const html = /* html */`
+<!DOCTYPE html>
+<html style=background:#000>
+  <head>
+    <meta http-equiv=refresh content="5; url=https://miguapp.pages.dev">
+    <meta property="og:title" content="Watch Together">
+    <meta property="og:description" content="Stream anime torrents, real-time with no waiting for downloads">
+    <meta property="og:site_name" content="Migu">
+    <meta property="og:image" content=https://miguapp.pages.dev/imgs/banner.png>
+    <meta property="og:url" content=migu://w2g/${id}>
+    <meta data-vmid="twitter:card" name="twitter:card" content="summary_large_image">
+    <meta name="theme-color" content="#17191C">
+  </head>
+  <body>
+    <iframe src=migu://w2g/${id} style=border:none></iframe>Redirecting...
+  </body>
+</html>`
+
+    return new Response(html, {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8'
+      }
+    })
+  } catch (e) {
+    return Response.redirect('https://miguapp.pages.dev/')
+  }
+}
