2f5d36c890d2efc8097b950ccff5942f376c247b
Zergiv <brayanhoyos1969@gmail.com>
2024-12-22 15:21:41 +0100

    Cositas varias 👍

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -21,30 +21,7 @@
 	<div
 		class="-10 flex  items-center justify-center"
 	>
-	<AnimatedGradientText>
-		🎉 <hr class="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />
-		{" "}
-		<span
-		  class={cn(
-			`inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
-		  )}
-		>
-		  Fork release
-		</span>
-		<svg
-		  xmlns="http://www.w3.org/2000/svg"
-		  width="24"
-		  height="24"
-		  viewBox="0 0 24 24"
-		  fill="none"
-		  stroke="currentColor"
-		  stroke-width="2"
-		  stroke-linecap="round"
-		  stroke-linejoin="round"
-		  class="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
-		  ><path d="m9 18 6-6-6-6" /></svg
-		>
-	  </AnimatedGradientText>
+	
 	</div>
 	<h1
 		class="-translate-y-4 animate-fade-in text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
