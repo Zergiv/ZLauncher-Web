752b530ffc1673b2f5df948a15c6489e31dd4871
Zergiv <brayanhoyos1969@gmail.com>
2024-12-28 22:16:24 +0100

    Update PricingSection.svelte

--- src/lib/components/landing/PricingSection.svelte
+++ src/lib/components/landing/PricingSection.svelte
@@ -126,7 +126,7 @@
 							<span
 								class="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform-gpu bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-96 dark:bg-black"
 							/>
-							Download
+							Descargar
 						</Button>
 					{:else}
 						<Button
@@ -140,7 +140,7 @@
 							<span
 								class="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform-gpu bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-96 dark:bg-black"
 							/>
-							Download
+							Descargar
 						</Button>
 					{/if}
 
